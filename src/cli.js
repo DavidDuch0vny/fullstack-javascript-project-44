58299ac3b4304a681e0794d2dec0166c19f77c3f
DavidDuch0vny <2813008oo4@gmail.com>
2023-02-11 14:58:14 +0100

    adding src.cli.js

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from "/mnt/c/users/maksy/onedrive/pulpit/fullstack-javascript-project-44/node_modules/readline-sync/lib/readline-sync.js";
+const welcome = () => {
+    const name = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${name}!`);
+}
+export default welcome;
