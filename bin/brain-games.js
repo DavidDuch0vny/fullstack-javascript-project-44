f6e64a8f003943e72c3b33334780db689e23886b
DavidDuch0vny <2813008oo4@gmail.com>
2023-02-14 01:09:15 +0100

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import welcome from '/mnt/c/users/maksy/onedrive/pulpit/fullstack-javascript-project-44/src/cli.js';
-import brainEven from '/mnt/c/users/maksy/onedrive/pulpit/fullstack-javascript-project-44/bin/brain-even.js';
+import { welcome } from '/mnt/c/users/maksy/onedrive/pulpit/fullstack-javascript-project-44/src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-
-const name = welcome();
-
-brainEven(name);
+welcome();
