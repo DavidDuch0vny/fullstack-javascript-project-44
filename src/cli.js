ff8337e3dc4f7e1406d567a1c7f62f8af975c9a4
DavidDuch0vny <2813008oo4@gmail.com>
2023-02-14 01:09:47 +0100

    adding functionality to cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,21 @@
 import readlineSync from '/mnt/c/users/maksy/onedrive/pulpit/fullstack-javascript-project-44/node_modules/readline-sync/lib/readline-sync.js';
 
-const welcome = () => {
+export function welcome() {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello,${name}!`);
   return name;
-};
+}
 
-export default welcome;
+export function check(name, result) {
+  let correct = true;
+  const answer = readlineSync.question('Your answer: ');
+  if (result.toString() === answer) {
+    console.log('Correct!');
+    return correct;
+  }
+  console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.`);
+  console.log(`Let's try again, ${name}!`);
+  correct = false;
+  return correct;
+}
