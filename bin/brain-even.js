5a5aae96f1582cae326898c5464d8bd75f9329e7
DavidDuch0vny <2813008oo4@gmail.com>
2023-02-14 01:08:42 +0100

    fix brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
-import readlineSync from '/mnt/c/users/maksy/onedrive/pulpit/fullstack-javascript-project-44/node_modules/readline-sync/lib/readline-sync.js';
+import { welcome, check } from '/mnt/c/users/maksy/onedrive/pulpit/fullstack-javascript-project-44/src/cli.js';
 
 const brainEven = (name) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
@@ -8,15 +8,10 @@ const brainEven = (name) => {
     const number = Math.floor(Math.random() * 100);
     const isEven = number % 2 === 0 ? 'yes' : 'no';
     console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (isEven === answer) console.log('Correct!');
-    else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${isEven}.`);
-      console.log(`Let's try again, ${name}!`);
-      correct = false;
-    }
+    correct = check(name, isEven);
     count += 1;
   }
 };
 
-export default brainEven;
+const name = welcome();
+brainEven(name);
